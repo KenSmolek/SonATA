632b2e0139e92dc80c5ec13214782cd51d980896
Ken Smolek <kes@smolek.com>
2011-04-19 11:36:10 -0700

    Created a Mersenne class to contain all the Mersenne Twister variables and functions.  A copy of the class is included in the Gaussian class, to keep separate copies of Gaussian completely independent of each other.

--- sig-pkg/gaussLib/include/Mersenne.h
+++ sig-pkg/gaussLib/include/Mersenne.h
@@ -32,7 +32,35 @@
 #ifndef _MersenneH
 #define _MersenneH
 
-void init_genrand64(unsigned long long seed);
-double genrand64_real2(void);
+namespace gauss {
 
-#endif
+#define NN 312
+#define MM 156
+#define MATRIX_A 0xB5026F5AA96619E9ULL
+#define UM 0xFFFFFFFF80000000ULL /* Most significant 33 bits */
+#define LM 0x7FFFFFFFULL /* Least significant 31 bits */
+
+
+
+class Mersenne {
+public:
+	Mersenne();
+
+	void init_genrand64(unsigned long long seed);
+	double genrand64_real2(void);
+
+private:
+	int mti;
+	unsigned long long mt[NN];	// array for state vector
+
+	void init_by_array64(unsigned long long init_key[],
+			unsigned long long key_length);
+	unsigned long long genrand64_int64(void);
+	long long genrand64_int63(void);
+	double genrand64_real1(void);
+	double genrand64_real3(void);
+};
+
+}
+
+#endif // _MersenneH
