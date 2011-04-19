83c8eef2a9b014fbe3793dab0aa6021762017a4c
Ken Smolek <kes@smolek.com>
2011-04-19 11:46:13 -0700

    Converted the code to a class called Mersenne.

--- sig-pkg/gaussLib/src/Mersenne.cpp
+++ sig-pkg/gaussLib/src/Mersenne.cpp
@@ -87,20 +87,15 @@
 #include <stdio.h>
 #include "Mersenne.h"
 
-#define NN 312
-#define MM 156
-#define MATRIX_A 0xB5026F5AA96619E9ULL
-#define UM 0xFFFFFFFF80000000ULL /* Most significant 33 bits */
-#define LM 0x7FFFFFFFULL /* Least significant 31 bits */
+namespace gauss {
 
+Mersenne::Mersenne(): mti(NN + 1)
+{
 
-/* The array for the state vector */
-static unsigned long long mt[NN];
-/* mti==NN+1 means mt[NN] is not initialized */
-static int mti=NN+1;
-
+}
 /* initializes mt[NN] with a seed */
-void init_genrand64(unsigned long long seed)
+void
+Mersenne::init_genrand64(unsigned long long seed)
 {
     mt[0] = seed;
     for (mti=1; mti<NN; mti++)
@@ -110,7 +105,8 @@ void init_genrand64(unsigned long long seed)
 /* initialize by an array with array-length */
 /* init_key is the array for initializing keys */
 /* key_length is its length */
-void init_by_array64(unsigned long long init_key[],
+void
+Mersenne::init_by_array64(unsigned long long init_key[],
 		unsigned long long key_length)
 {
     unsigned long long i, j, k;
@@ -135,7 +131,8 @@ void init_by_array64(unsigned long long init_key[],
 }
 
 /* generates a random number on [0, 2^64-1]-interval */
-unsigned long long genrand64_int64(void)
+unsigned long long
+Mersenne::genrand64_int64(void)
 {
     int i;
     unsigned long long x;
@@ -173,25 +170,29 @@ unsigned long long genrand64_int64(void)
 }
 
 /* generates a random number on [0, 2^63-1]-interval */
-long long genrand64_int63(void)
+long long
+Mersenne::genrand64_int63(void)
 {
     return (long long)(genrand64_int64() >> 1);
 }
 
 /* generates a random number on [0,1]-real-interval */
-double genrand64_real1(void)
+double
+Mersenne::genrand64_real1(void)
 {
     return (genrand64_int64() >> 11) * (1.0/9007199254740991.0);
 }
 
 /* generates a random number on [0,1)-real-interval */
-double genrand64_real2(void)
+double
+Mersenne::genrand64_real2(void)
 {
     return (genrand64_int64() >> 11) * (1.0/9007199254740992.0);
 }
 
 /* generates a random number on (0,1)-real-interval */
-double genrand64_real3(void)
+double
+Mersenne::genrand64_real3(void)
 {
     return ((genrand64_int64() >> 12) + 0.5) * (1.0/4503599627370496.0);
 }
@@ -215,3 +216,5 @@ int main(void)
     return 0;
 }
 #endif
+
+}
